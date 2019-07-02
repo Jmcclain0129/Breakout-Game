0723e7d298e46234302f8415e83529a94ef3fda3
Jmcclain0129 <jmcclain0129@gmail.com>
2019-07-02 13:32:46 +0100

    first commit

---
+++ src/paddle.js
@@ -0,0 +1,27 @@
+export default class Paddle {
+
+    constructor(gameWidth, gameHeight) {
+        this.width = 150;
+        this.height = 30;
+
+
+        this.position = {
+
+            x: gameWidth / 2 - this.width / 2,
+            y: gameHeight - this.height - 10,
+
+        }
+    }
+
+    draw(ctx) {
+        ctx.fillStyle = '#off';
+        ctx.fillRect(this.position.x, this.position.y, this.width, this.height);
+
+    }
+}
+update(deltaTime); {
+
+    if(!deltaTime) return;
+
+
+}
